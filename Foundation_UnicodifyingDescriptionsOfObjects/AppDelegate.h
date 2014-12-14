65d3127d5ef0c2ec0431e82ee66dab432b72a5d9
Xaree Lee <xareelee@gmail.com>
2014-12-15 03:48:02 +0800

    Initial Commit

---
+++ Foundation_UnicodifyingDescriptionsOfObjects/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foundation_UnicodifyingDescriptionsOfObjects
+//
+//  Created by Xaree on 12/15/14.
+//  Copyright (c) 2014 Xaree Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Foundation_UnicodifyingDescriptionsOfObjects/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Foundation_UnicodifyingDescriptionsOfObjects
+//
+//  Created by Xaree on 12/15/14.
+//  Copyright (c) 2014 Xaree Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
